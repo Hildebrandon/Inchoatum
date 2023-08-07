5f28bce2ac962c2d1718fdb48a12264d626f907a
Hildebrandon <hildebrandon@gmail.com>
2023-08-07 12:14:32 -0500

    Altered comments

--- grade_data.py
+++ grade_data.py
@@ -9,6 +9,7 @@ import statistics # needed for mean, median, and standard deviation
 
 
 # define functions to return mean, median, and mode
+
 def find_mean(input):
     num_mean = statistics.mean(input)
     round_num_mean = format(num_mean, ".2f")
@@ -24,7 +25,8 @@ def find_std(input):
     round_num_std = format(num_std, ".2f")
     return round_num_std
 
-# open the csv file
+# open the csv file using with command
+
 with open("sample_grades.csv", "r") as csv_file:
     # specify field names
     field_names = ["Name", "Semester", "Grade"]
@@ -32,27 +34,34 @@ with open("sample_grades.csv", "r") as csv_file:
     csv_reader = csv.DictReader(csv_file, fieldnames=field_names)  # fieldnames parameter tells what the field names are
 
     # create a list to store the dictionaries
+
     data = []
 
     # loop through each row
+
     for row in csv_reader:
         data.append(row)
 
 # initialize all values    
+
     fall_grades = []
     spring_grades = []
 
 # loop over dictionary entries in list of data
+
 for entry in data:
     # check values for second key-value pair to see what semester
+
     sem_value = entry["Semester"]
     # loop to populate lists for grades
+
     if sem_value == "Fall 2016":
         fall_grades.append(int(entry["Grade"]))
     if sem_value == "Spring 2016":
         spring_grades.append(int(entry["Grade"]))
 
 # output values in chart, ANSI hero
+
 header = "\033[1m{:^12} {:^12} {:^12}\033[0m".format("", "Fall 2016", "Spring 2016")
 print(header)
 print("\033[1m{:<12}\033[0m {:^12} {:^12}".format("Mean:", find_mean(fall_grades) , find_mean(spring_grades)))
@@ -60,6 +69,7 @@ print("\033[1m{:<12}\033[0m {:^12} {:^12}".format("Median:", find_median(fall_gr
 print("\033[1m{:<12}\033[0m {:^12} {:^12}".format("STD:", find_std(fall_grades) , find_std(spring_grades)))
 
 # # output values in chart; spacebar hero
+
 # print(f"            \033[1mFall 2016       Spring 2016\033[0m")
 # print(f"\033[1mMean:\033[0m         {find_mean(fall_grades)}            {find_mean(spring_grades)}")
 # print(f"\033[1mMedian:\033[0m       {find_median(fall_grades)}            {find_median(spring_grades)}")
